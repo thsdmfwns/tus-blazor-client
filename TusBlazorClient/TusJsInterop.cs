568770ef71961a6d4a24d124c8fc3f187cae6e72
son <son2044@gmail.com>
2023-10-23 12:48:40 +0900

    add nuget setting and update README.md and fix warnings

--- TusBlazorClient/TusJsInterop.cs
+++ TusBlazorClient/TusJsInterop.cs
@@ -27,13 +27,13 @@ public class TusJsInterop : IAsyncDisposable
     public async ValueTask<bool> IsSupported()
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<bool>("IsSupported");
+        return await _script!.InvokeAsync<bool>("IsSupported");
     }
     
     public async ValueTask<bool> CanStoreUrls()
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<bool>("CanStoreUrls");
+        return await _script!.InvokeAsync<bool>("CanStoreUrls");
     }
 
     public async ValueTask<IJSObjectReference> GetUpload(
@@ -44,7 +44,7 @@ public class TusJsInterop : IAsyncDisposable
     )
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<IJSObjectReference>("GetUpload", fileObjectRef, options, jsInvokeReference, tusOptionNullCheck);
+        return await _script!.InvokeAsync<IJSObjectReference>("GetUpload", fileObjectRef, options, jsInvokeReference, tusOptionNullCheck);
     }
 
     public async ValueTask<IJSObjectReference> GetUploadByJsFileRef(
@@ -55,49 +55,49 @@ public class TusJsInterop : IAsyncDisposable
     )
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<IJSObjectReference>("GetUploadByJsFileRef", file, options, jsInvokeReference, tusOptionNullCheck);
+        return await _script!.InvokeAsync<IJSObjectReference>("GetUploadByJsFileRef", file, options, jsInvokeReference, tusOptionNullCheck);
     }
 
     public async ValueTask<int> GetFileListLength(ElementReference element)
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<int>("GetFileListLength", element);
+        return await _script!.InvokeAsync<int>("GetFileListLength", element);
     }
     
     internal async ValueTask<IJSObjectReference> GetFile(JsFile jsFile)
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<IJSObjectReference>("GetFile", jsFile);
+        return await _script!.InvokeAsync<IJSObjectReference>("GetFile", jsFile);
     }
     
     internal async ValueTask<JsFileInfo> GetFileInfoFromJsFileRef(JsFile jsFile)
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<JsFileInfo>("GetFileInfoFromJsFileRef", jsFile);
+        return await _script!.InvokeAsync<JsFileInfo>("GetFileInfoFromJsFileRef", jsFile);
     }
 
     public async ValueTask<IJSObjectReference> GetFileFromUpload(IJSObjectReference tusUploadJsObjectReference)
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<IJSObjectReference>("GetFileFromUpload", tusUploadJsObjectReference);
+        return await _script!.InvokeAsync<IJSObjectReference>("GetFileFromUpload", tusUploadJsObjectReference);
     }
 
     public async ValueTask<JsFileInfo> GetFileInfo(IJSObjectReference fileJsObjectReference)
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<JsFileInfo>("GetFileInfo", fileJsObjectReference);
+        return await _script!.InvokeAsync<JsFileInfo>("GetFileInfo", fileJsObjectReference);
     }
 
     public async ValueTask<string> GeUrlFromUpload(IJSObjectReference tusUploadJsObjectReference)
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<string>("GeUrlFromUpload", tusUploadJsObjectReference);
+        return await _script!.InvokeAsync<string>("GeUrlFromUpload", tusUploadJsObjectReference);
     }
 
     public async ValueTask<TusOptions> GetOptionFromUpload(IJSObjectReference tusUploadJsObjectReference)
     {
         await InitializeAsync();
-        return await _script.InvokeAsync<TusOptions>("GetOptionFromUpload", tusUploadJsObjectReference);
+        return await _script!.InvokeAsync<TusOptions>("GetOptionFromUpload", tusUploadJsObjectReference);
     }
 
     public async Task SetTusUploadOption(
@@ -107,7 +107,7 @@ public class TusJsInterop : IAsyncDisposable
         TusOptionNullCheck tusOptionNullCheck)
     {
         await InitializeAsync();
-        await _script.InvokeVoidAsync("SetTusUploadOption",
+        await _script!.InvokeVoidAsync("SetTusUploadOption",
             tusUploadJsObject, tusOptions,
             tusOptionJsInvokeObject, tusOptionNullCheck);
     }
@@ -115,7 +115,7 @@ public class TusJsInterop : IAsyncDisposable
     public async Task ResumeFromPreviousUpload(IJSObjectReference tusUploadJsObject, int index)
     {
         await InitializeAsync();
-        await _script.InvokeVoidAsync("resumeFromPreviousUpload", tusUploadJsObject, index);
+        await _script!.InvokeVoidAsync("resumeFromPreviousUpload", tusUploadJsObject, index);
     }
 
     public async ValueTask DisposeAsync()
